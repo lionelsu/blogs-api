10896a52a9d24e04b53ee8c5d0bf53ec5abc0087
Lionel Miranda <bibliothecalionel@gmail.com>
2023-08-27 19:05:33 -0400

    feat: db sequelize migrations

---
+++ src/migrations/20230827191245-create-posts_categories.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
